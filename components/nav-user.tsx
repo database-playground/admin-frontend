37ff4a0d2d232eec4148a1f4faa15bf0d2d86ab6
Yi-Jyun Pan <me@pan93.com>
2025-09-14 02:38:29 +0800

    style: format codebase

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -11,12 +11,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import {
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  useSidebar,
-} from "@/components/ui/sidebar";
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar";
 import Link from "next/link";
 import AppAvatar from "./avatar";
 
