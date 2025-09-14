946690c6bf87f59a33105ae2b1ee67feb01e7d94
Yi-Jyun Pan <me@pan93.com>
2025-09-14 20:26:41 +0800

    style: reformat code

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,6 +1,6 @@
-import { unstable_ViewTransition as ViewTransition } from "react";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { unstable_ViewTransition as ViewTransition } from "react";
 
 export default function AdminLayout({
   children,
@@ -9,12 +9,10 @@ export default function AdminLayout({
 }>) {
   return (
     <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
+      style={{
+        "--sidebar-width": "calc(var(--spacing) * 72)",
+        "--header-height": "calc(var(--spacing) * 12)",
+      } as React.CSSProperties}
     >
       <AppSidebar variant="inset" />
       <SidebarInset>
