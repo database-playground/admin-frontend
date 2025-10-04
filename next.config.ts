51e488aa233d5b742eb3715e6ac6b700b064b4ae
Yi-Jyun Pan <me@pan93.com>
2025-10-05 03:10:29 +0800

    fix: react compiler is not experimental now
    I also removed an unused "turbopackPersistentCaching"
    which is renamed in Next.js 15.6 (see vercel/next.js#84215).

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  reactCompiler: true,
   experimental: {
-    reactCompiler: true,
     viewTransition: true,
     swcPlugins: [
       ["@swc-contrib/plugin-graphql-codegen-client-preset", { artifactDirectory: "./gql", gqlTagName: "graphql" }],
     ],
     ppr: "incremental",
-    turbopackPersistentCaching: true,
     authInterrupts: true,
   },
 };
