84f4f374d17c0cbae512be538df69f3dc5d147c5
Yi-Jyun Pan <me@pan93.com>
2025-10-19 04:44:18 +0800

    fix(home): header typo

--- app/(admin)/_components/header.tsx
+++ app/(admin)/_components/header.tsx
@@ -10,7 +10,7 @@ export function Header() {
       <div>
         <h2 className="text-2xl font-bold tracking-tight">哈囉，{user.name}！</h2>
         <p className="text-muted-foreground">
-          這裡可以快速總覽所有統計資料，也可以點選左邊的側邊來進行資料管理。
+          這裡可以快速總覽所有統計資料，也可以點選左邊的側邊欄來進行資料管理。
         </p>
       </div>
     </div>
